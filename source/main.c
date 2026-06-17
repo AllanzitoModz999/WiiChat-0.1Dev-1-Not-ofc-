9d0907588c8ea46136079c9db97434626d63cbcf
𓂀𝔸𝕝𝕝𝕒𝕟 :)𓂀 <allangabriel10092@gmail.com>
2026-06-17 20:11:51 -0300

    Atualizar o main.c

--- source/main.c
+++ source/main.c
@@ -1,44 +1,84 @@
 #include <gccore.h>
 #include <grrlib.h>
 #include <wiiuse/wpad.h>
+#include <ogc/system.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 #include "ui.h"
+#include "pointer.h"
+
+// Estado do programa
+bool running = true;
 
 int main(int argc, char **argv)
 {
+    // Inicializa vídeo
     VIDEO_Init();
+
+    // Inicializa Wii Remote
     WPAD_Init();
 
-    // Ativa o IR (ponteiro do Wii)
+    // Ativa botões + acelerômetro + IR
     WPAD_SetDataFormat(WPAD_CHAN_0, WPAD_FMT_BTNS_ACC_IR);
 
-    // Inicializa o GRRLIB
+    // Inicializa GRRLIB
     GRRLIB_Init();
 
-    // Inicializa a interface
+    // Inicializa interface
     UI_Init();
 
-    while (1)
+    while (running)
     {
+        // Atualiza controles
         WPAD_ScanPads();
 
+        // Atualiza posição do ponteiro
+        Pointer_Update();
+
+        // Botões pressionados
         u32 pressed = WPAD_ButtonsDown(0);
 
-        // HOME = sair do aplicativo
+        //-------------------------------------------------
+        // HOME = sair do WiiChat
+        //-------------------------------------------------
         if (pressed & WPAD_BUTTON_HOME)
         {
-            break;
+            running = false;
         }
 
-        // Desenha toda a interface
+        //-------------------------------------------------
+        // Limpa a tela
+        //-------------------------------------------------
+        GRRLIB_FillScreen(0xFFFFFFFF);
+
+        //-------------------------------------------------
+        // Desenha interface
+        //-------------------------------------------------
         UI_Draw();
+
+        //-------------------------------------------------
+        // Desenha cursor
+        //-------------------------------------------------
+        Pointer_Draw();
+
+        //-------------------------------------------------
+        // Renderiza
+        //-------------------------------------------------
+        GRRLIB_Render();
 
         VIDEO_WaitVSync();
     }
 
-    // Finaliza o GRRLIB
+    //-----------------------------------------------------
+    // Finalização
+    //-----------------------------------------------------
+
     GRRLIB_Exit();
+
+    WPAD_Shutdown();
+
+    SYS_ResetSystem(SYS_RETURNTOMENU, 0, 0);
 
     return 0;
 }
