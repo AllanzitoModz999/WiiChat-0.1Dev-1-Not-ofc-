5bfa0cc87824ff704721226284c4f5c2a5ccb5f6
𓂀𝔸𝕝𝕝𝕒𝕟 :)𓂀 <allangabriel10092@gmail.com>
2026-06-17 20:15:28 -0300

    Atualizar o pointer.h

--- source/pointer.h
+++ source/pointer.h
@@ -3,9 +3,20 @@
 
 #include <wiiuse/wpad.h>
 
-extern ir_t ir;
+// Inicializa o cursor
+void Pointer_Init(void);
 
+// Atualiza a posição do cursor
 void Pointer_Update(void);
+
+// Desenha o cursor
 void Pointer_Draw(void);
+
+// Retorna posição atual
+float Pointer_GetX(void);
+float Pointer_GetY(void);
+
+// Retorna se o IR está válido
+bool Pointer_IsValid(void);
 
 #endif
