6deae352d21870d11bd8b568f1e4cc2bf792dd3b
𓂀𝔸𝕝𝕝𝕒𝕟 :)𓂀 <allangabriel10092@gmail.com>
2026-06-17 20:15:06 -0300

    Atualizar o pointer.c

--- source/pointer.c
+++ source/pointer.c
@@ -1,9 +1,19 @@
 #include <grrlib.h>
 #include <wiiuse/wpad.h>
+#include <stdbool.h>
 
 #include "pointer.h"
 
-ir_t ir;
+// Arquivo gerado automaticamente pelo Makefile
+#include "hand-pointer-blue.h"
+
+static ir_t ir;
+static GRRLIB_texImg *cursor = NULL;
+
+void Pointer_Init(void)
+{
+    cursor = GRRLIB_LoadTexture(hand_pointer_blue_png);
+}
 
 void Pointer_Update(void)
 {
@@ -12,15 +22,32 @@ void Pointer_Update(void)
 
 void Pointer_Draw(void)
 {
-    if (!ir.valid)
+    if (!ir.valid || cursor == NULL)
         return;
 
-    // Cursor temporário
-    GRRLIB_Circle(
+    // Desenha a mão
+    GRRLIB_DrawImg(
         ir.x,
         ir.y,
-        8,
-        0x000000FF,
-        true
+        cursor,
+        0.0f,
+        1.0f,
+        1.0f,
+        0xFFFFFFFF
     );
+}
+
+float Pointer_GetX(void)
+{
+    return ir.x;
+}
+
+float Pointer_GetY(void)
+{
+    return ir.y;
+}
+
+bool Pointer_IsValid(void)
+{
+    return ir.valid;
 }
